ae5a137acd4703cab9abc091376a8afbfdd0b2f6
Danilo <dsv@dcc.ufrj.br>
2023-08-14 17:42:08 -0300

    Adicionando página de login

--- landing-page-gakalu/src/index.tsx
+++ landing-page-gakalu/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Login from "../src/pages/Login"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     {/* <App /> */}
+    <Login/>
   </React.StrictMode>
 );
 
